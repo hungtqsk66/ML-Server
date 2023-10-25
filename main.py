c5e30c64e70dcaeb7948bb78f3cfb7382ed66469
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-10-25 10:59:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nth-audio.site","http://127.0.0.1:5500"],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
