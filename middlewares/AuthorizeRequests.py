f239aa547b63e2af37d1aaa46032e60c441dec3e
hungtqsk66 <nguyenthanhhungso@gmail.com>
2024-05-07 22:17:37 +0700

    Update codes

--- middlewares/AuthorizeRequests.py
+++ middlewares/AuthorizeRequests.py
@@ -1,7 +1,8 @@
 from fastapi import  Request
 from utils.ResponseTypes import ErrorResponse
-from db.mongodb.init import MongoDB
+from db.redis.init import RedisDB
 from fastapi import status
+import json
 
 
 async def AuthorizeAccess(request:Request,call_next):
@@ -10,18 +11,18 @@ async def AuthorizeAccess(request:Request,call_next):
     if fromSameOrigin :  
         return await call_next(request)
     
-    collection =  MongoDB.getInstance()['audioServerDev']['ApiKeys']
+    cache =  RedisDB.getInstance()
     api_key = request.headers.get('x-api-key')
     
     if api_key is None: 
-        return ErrorResponse(message="",code=status.HTTP_400_BAD_REQUEST)
+        return ErrorResponse(message="No valid credential",code=status.HTTP_400_BAD_REQUEST)
     
-    doc = await collection.find_one({'key':api_key})
+    doc = await cache.get(api_key)
     
     if doc is None : 
         return ErrorResponse(message="Forbidden",code=status.HTTP_403_FORBIDDEN)
     
-    permissions = list(doc['permissions'])
+    permissions:list = json.loads(doc)
     
     if not ("GENERAL" in permissions) : 
         return ErrorResponse(message="You don't have permission",code=status.HTTP_401_UNAUTHORIZED)
