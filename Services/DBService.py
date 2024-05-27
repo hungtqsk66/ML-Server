04e567a38eb6c7adab9d11fc53cf9fe342a153c0
hungtqsk66 <nguyenthanhhungso@gmail.com>
2024-05-28 06:40:14 +0700

    Update DBService.py

--- Services/DBService.py
+++ Services/DBService.py
@@ -2,7 +2,7 @@ from db.mongodb.init import MongoDB
 from bson.objectid import ObjectId
 from pandas import DataFrame
 from typing import Optional
-import jwt# type: ignore
+from jose import jwt# type: ignore
 from decouple import config #type:ignore
 
 class DBService():
